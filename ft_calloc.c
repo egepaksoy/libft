e536b162391299364d08e2a570f4627e66900f5b
Ege Paksoy <egepaskoy@gmail.com>
2023-07-07 17:12:36 +0300

    calloc ok

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epaksoy <epaksoy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/07 15:34:58 by epaksoy           #+#    #+#             */
+/*   Updated: 2023/07/07 17:12:17 by epaksoy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include    "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*add;
+
+	add = malloc(count * size);
+	if (!add)
+		return (add);
+	ft_bzero(add, count * size);
+	return (add);
+}
