1829e256e45924be03a37921b1fc91f7be447e2f
Ege Paksoy <egepaskoy@gmail.com>
2023-07-07 16:02:53 +0300

    mem'ler bitti

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,16 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: epaksoy <epaksoy@student.42kocaeli.com.tr  +#+  +:+       +#+        */
+/*   By: epaksoy <epaksoy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 17:12:42 by epaksoy           #+#    #+#             */
-/*   Updated: 2023/07/06 17:34:03 by epaksoy          ###   ########.fr       */
+/*   Updated: 2023/07/07 15:54:07 by epaksoy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include    "libft.h"
 
-void *ft_memove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
+	size_t	i;
 
+	i = 0;
+	if (!dst && !src)
+		return (NULL);
+	if (dst < src)
+		ft_memcpy(dst, src, len);
+	else
+	{
+		while (len < i)
+		{
+			*(unsigned char *)(dst + i) = *(unsigned char *)(src + i);
+			i--;
+		}
+	}
+	return ((void *)dst);
 }
