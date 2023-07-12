a97c0bfadc915747a4f1d7deb76f99c3ad438b67
Ege Paksoy <egepaskoy@gmail.com>
2023-07-12 17:30:50 +0300

    strmapi bitti

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: epaksoy <epaksoy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 16:32:05 by epaksoy           #+#    #+#             */
-/*   Updated: 2023/07/12 16:53:24 by epaksoy          ###   ########.fr       */
+/*   Updated: 2023/07/12 17:28:46 by epaksoy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 
 #endif
