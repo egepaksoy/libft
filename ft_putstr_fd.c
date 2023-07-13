98655f3953c451a38aa685e0a76636939670e3e7
Ege Paksoy <egepaskoy@gmail.com>
2023-07-13 19:17:45 +0300

    ft_putstr_fd bitti

---
+++ ft_putstr_fd.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epaksoy <epaksoy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 19:15:50 by epaksoy           #+#    #+#             */
+/*   Updated: 2023/07/13 19:17:14 by epaksoy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		write(fd, "(NULL)", 6);
+	else
+		write(fd, s, ft_strlen(s));
+}
+
+// ft_strlen fonksiyonundan donen byte sayısı kadar
+// stringi fd descriptoru ile yazdırıyor
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: epaksoy <epaksoy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 16:32:05 by epaksoy           #+#    #+#             */
-/*   Updated: 2023/07/13 19:11:20 by epaksoy          ###   ########.fr       */
+/*   Updated: 2023/07/13 19:17:26 by epaksoy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,6 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
